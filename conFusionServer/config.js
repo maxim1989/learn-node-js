e88fda9d09bfc6f4c03190e5f4eee3803d366909
Maksim <fedorov-max89@mail.ru>
2021-05-29 21:36:59 +0300

    git reset HEAD

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '286125316581003',
+        clientSecret: '93fde9c3c9f7ca6c3b2ca1b0f35d2fa0'
+    }
 }
