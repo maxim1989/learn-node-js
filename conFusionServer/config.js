21cb66452295055288e1ba12f71daec95f821752
Maksim <fedorov-max89@mail.ru>
2021-05-29 21:38:47 +0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '286125316581003',
-        clientSecret: '93fde9c3c9f7ca6c3b2ca1b0f35d2fa0'
+        clientId: '***',
+        clientSecret: '***'
     }
 }
